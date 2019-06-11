4ad58a3f4a51d735c29db7e2046709bcfb7d87a6
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-06-11 13:24:46 -0700

    Completed closure stretch goals

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,6 +2,8 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
+const duplicates = ['Xbox', 'PS4', 'Xbox', 'PC', 'Nintendo Switch', 'PS4'];
+
 /* 
 
   //Given this problem: 
@@ -70,4 +72,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -23,7 +23,20 @@ hello();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+
+  return function() {
+    count++;
+    return count;
+  }
 };
+
+const newCounter = counter();
+
+newCounter();
+newCounter();
+newCounter();
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -33,4 +46,23 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  let count = 0;
+
+  return {
+    increment: function() {
+      return count+=1;
+    },
+    decrement: function() {
+      return count-=1;
+    }
+  }
 };
+
+let counterObject = counterFactory();
+
+console.log(counterObject.increment());
+console.log(counterObject.increment());
+console.log(counterObject.increment());
+console.log(counterObject.decrement());
+console.log(counterObject.decrement());
