39438e2576d6bb504a9b626e5b0c75e4178c85ea
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-06-11 12:38:03 -0700

    Completed closure assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let greeting = 'Hello my name is '
+
+function hello() {
+    let firstName = 'George ';
+
+    function yo(){
+      let lastName = 'Washington';
+      return console.log(greeting + firstName + lastName);
+    }
+
+    return yo();
+}
+
+hello();
+
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
